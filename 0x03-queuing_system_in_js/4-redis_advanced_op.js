fdcedebcafa43d8b743a22dad3660cdfcaa94092
Cyril-777 <kyrillosmaher10@gmail.com>
2023-11-27 19:20:09 +0000

    Task 4 - fix 'print'

--- 0x03-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -10,12 +10,12 @@ client.on('error', (error) => {
   console.error(`Redis client not connected to the server: ${error}`);
 });
 
-client.hset('HolbertonSchools', 'Portland', '50', print);
-client.hset('HolbertonSchools', 'Seattle', '80', print);
-client.hset('HolbertonSchools', 'New York', '20', print);
-client.hset('HolbertonSchools', 'Bogota', '20', print);
-client.hset('HolbertonSchools', 'Cali', '40', print);
-client.hset('HolbertonSchools', 'Paris', '2', print);
+client.hset('HolbertonSchools', 'Portland', '50', redis.print);
+client.hset('HolbertonSchools', 'Seattle', '80', redis.print);
+client.hset('HolbertonSchools', 'New York', '20', redis.print);
+client.hset('HolbertonSchools', 'Bogota', '20', redis.print);
+client.hset('HolbertonSchools', 'Cali', '40', redis.print);
+client.hset('HolbertonSchools', 'Paris', '2', redis.print);
 
 client.hgetall('HolbertonSchools', (error, value) => {
   if (error) {
